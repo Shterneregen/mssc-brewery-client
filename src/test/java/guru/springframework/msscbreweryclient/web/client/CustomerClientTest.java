62df3a12e7f819d7c3fd078cd2aa076e4c2e0156
shterneregen <shterneregen@gmail.com>
2020-01-30 23:56:24 +0300

    restTemplate methods for Client endpoints

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/CustomerClientTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CustomerClientTest {
+
+    @Autowired
+    private CustomerClient client;
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        URI uri = client.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
+    }
+}
