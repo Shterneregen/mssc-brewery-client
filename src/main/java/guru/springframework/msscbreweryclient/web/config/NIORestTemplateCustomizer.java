75652bdd9ea19f8121736caa9596f16938cbda14
shterneregen <shterneregen@gmail.com>
2020-01-31 11:46:06 +0300

    Apache HTTP client configuration

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/NIORestTemplateCustomizer.java
@@ -0,0 +1,50 @@
+package guru.springframework.msscbreweryclient.web.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
+import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.nio.reactor.IOReactorException;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+//@Component
+public class NIORestTemplateCustomizer implements RestTemplateCustomizer {
+
+    public ClientHttpRequestFactory clientHttpRequestFactory() throws IOReactorException {
+        final DefaultConnectingIOReactor ioReactor = new DefaultConnectingIOReactor(IOReactorConfig.custom()
+                .setConnectTimeout(3000)
+                .setIoThreadCount(4)
+                .setSoTimeout(3000)
+                .build());
+
+        final PoolingNHttpClientConnectionManager connectionManager
+                = new PoolingNHttpClientConnectionManager(ioReactor);
+        connectionManager.setDefaultMaxPerRoute(100);
+        connectionManager.setMaxTotal(1000);
+
+        CloseableHttpAsyncClient httpAsyncClient = HttpAsyncClients.custom()
+                .setConnectionManager(connectionManager)
+                .build();
+
+        return new HttpComponentsAsyncClientHttpRequestFactory(httpAsyncClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        log.info("===================================================================================================");
+        log.info("NIORestTemplateCustomizer");
+        log.info("===================================================================================================");
+        try {
+            restTemplate.setRequestFactory(clientHttpRequestFactory());
+        } catch (IOReactorException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
